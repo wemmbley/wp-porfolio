d5f0309e055004a8f216209bbebb12253c1d9de4
wemmbley <golevv.rustam@gmail.com>
2019-11-10 15:13:49 +0200

    added blog

---
+++ wp-content/themes/portfolio/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+try {
+    require_once 'vendor/autoload.php';
+
+    $app_config = [
+        \Portfolio\App\Assets\Assets::class,
+    ];
+
+    $boot = \Portfolio\App\Bootstrap::load( $app_config );
+} catch ( Exception $exception ) {
+    if( WP_DISPLAY_ERRORS ) {
+        echo $exception->getMessage();
+        exit;
+    }
+
+    error_log( $exception->getMessage() );
+}
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,43 +1,13 @@
 <?php
 
-try {
-    require_once 'vendor/autoload.php';
-
-    $app_config = [
-        \Portfolio\App\Assets\Assets::class,
-    ];
-
-    $boot = \Portfolio\App\Bootstrap::load( $app_config );
-
-    ?>
-    <!DOCTYPE html>
-        <html lang="en" class="no-js">
-            <body>
-    <?php
-    get_header();
-
-    require_once 'blocks/header-block.php';
-    require_once 'blocks/banner-block.php';
-    require_once 'blocks/about-block.php';
-    require_once 'blocks/services-block.php';
-    require_once 'blocks/fact-block.php';
-    require_once 'blocks/porfolio-block.php';
-    require_once 'blocks/testimonial-block.php';
-    require_once 'blocks/price-block.php';
-    require_once 'blocks/blog-block.php';
-    require_once 'blocks/brands-block.php';
-
-    get_footer();
-    ?>
-            </body>
-    </html>
-    <?
-
-} catch ( Exception $exception ) {
-    if( WP_DISPLAY_ERRORS ) {
-        echo $exception->getMessage();
-        exit;
-    }
-
-    error_log( $exception->getMessage() );
-}
+get_header();
+require_once 'blocks/banner-block.php';
+require_once 'blocks/about-block.php';
+require_once 'blocks/services-block.php';
+require_once 'blocks/fact-block.php';
+require_once 'blocks/porfolio-block.php';
+require_once 'blocks/testimonial-block.php';
+require_once 'blocks/price-block.php';
+require_once 'blocks/blog-block.php';
+require_once 'blocks/brands-block.php';
+get_footer();
